a75c4fc46a14122ba78b31041c23135da848c632
Rafael Chica <rafael.chica@sqasa.co>
2024-05-08 16:51:05 -0500

    proyecto pom

---
+++ src/main/java/com/exito/pageObject/HomePageObject.java
@@ -0,0 +1,15 @@
+package com.exito.pageObject;
+/*
+ * @(#) HomePageObject.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import net.serenitybdd.core.pages.PageObject;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class HomePageObject extends PageObject {
+
+}
---
+++ src/test/java/com/exito/stepDefinitions/AddProductStepDefinitions.java
@@ -0,0 +1,40 @@
+package com.exito.stepDefinitions;
+/*
+ * @(#) AddProductStepDefinitions.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import io.cucumber.java.en.*;
+
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class AddProductStepDefinitions {
+
+    @Given("que el usuario ingresa a la pagina web")
+    public void queElUsuarioIngresaALaPaginaWeb() {
+
+    }
+    @When("el usuario ingresa al menu")
+    public void elUsuarioIngresaAlMenu() {
+
+    }
+    @When("Busca la categoria y subcategoria")
+    public void buscaLaCategoriaYSubcategoria() {
+
+    }
+    @When("Selecciona el producto")
+    public void seleccionaElProducto() {
+
+    }
+    @When("Dar click en el boton de agregar al carrito")
+    public void darClickEnElBotonDeAgregarAlCarrito() {
+
+    }
+    @Then("Se podra visualizar el producto en el carrito de compras")
+    public void sePodraVisualizarElProductoEnElCarritoDeCompras() {
+
+    }
+}
