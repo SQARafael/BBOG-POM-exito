5cbe5507d2e4bfd3697695e2352866ace47b684c
Rafael Chica <rafael.chica@sqasa.co>
2024-05-21 08:42:07 -0500

    add read data excel

---
+++ src/main/java/com/exito/utils/CallData.java
@@ -0,0 +1,20 @@
+package com.exito.utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class CallData {
+
+    private  static ArrayList<Map<String, String>> data= new ArrayList<>();
+
+    public static ArrayList<Map<String, String>> extractTo(){
+        try {
+            data= ExcelCode.leerDatosDeHojaDeExcel("src/main/resources/Data/search.xlsx","productos");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return data;
+    }
+}
